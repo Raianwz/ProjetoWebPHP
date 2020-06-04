79293fff3eed153caae77005cdb16834e5f259b3
raianwz <raian03@live.com>
2020-06-04 20:34:28 -0300

    Adicionado CSS e Novos Cards

--- galeria.php
+++ galeria.php
@@ -1,26 +1,9 @@
-<?php
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-    <title>Clorocine</title>
-</head>
-
+<?php include "cabecalho.php" ?>
 <body>
     <nav class="nav-extended purple lighten-3">
         <div class="nav-wrapper">
             <ul id="nav-mobile" class="right">
-                <li><a href="galeria.php">Galeria</a></li>
+                <li class="active"><a href="galeria.php">Galeria</a></li>
                 <li><a href="cadastrar.php">Cadastrar</a></li>
             </ul>
         </div>
@@ -37,7 +20,7 @@
     </nav>
 
 <div class="row">
-
+<!-- Coluna Geral -->
 
     <div class="col s3">
         <div class="card hoverable">
@@ -64,7 +47,7 @@
           </div>
           <div class="card-content">
               <p class="valign-wrapper">
-                  <i class="material-icons amber-text">star</i>9,7</p>
+                  <i class="material-icons amber-text">star</i>6,6</p>
                   <span class="card-title">AD Astra</span>
             <p>Roy McBride é um engenheiro espacial, que decide empreender 
                 a maior jornada de sua vida: viajar para o espaço, um astronauta que se perdeu há vinte anos atrás no caminho para Netuno.</p>
@@ -72,6 +55,47 @@
         </div>
     </div>
 
+    <div class="col s3">
+        <div class="card hoverable">
+          <div class="card-image">
+            <img src="https://image.tmdb.org/t/p/w300/lFx2i2pg1BoaD7grcpGDyHM1eML.jpg">
+            
+            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+          </div>
+          <div class="card-content">
+              <p class="valign-wrapper">
+                  <i class="material-icons amber-text">star</i>6,7</p>
+                  <span class="card-title">Star Wars: Episódio IX – A Ascensão Skywalker</span>
+            <p>Com o retorno do Imperador Palpatine, a Resistência toma a frente da batalha que ditará os rumos da galáxia. 
+            Treinando para ser uma completa Jedi, Rey ainda se encontra em conflito com seu passado e futuro, mas teme pelas 
+            respostas que pode conseguir a partir de sua complexa ligação com Kylo Ren, que também se encontra em conflito pela Força.</p>
+          </div>
+        </div>
+    </div>
+
+    <div class="col s3">
+        <div class="card hoverable">
+          <div class="card-image">
+            <img src="https://image.tmdb.org/t/p/w300/18iq63BiWzdY529vGDeGVUpcSWT.jpg">
+            
+            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+          </div>
+          <div class="card-content">
+              <p class="valign-wrapper">
+                  <i class="material-icons amber-text">star</i>7,9</p>
+                  <span class="card-title">Liga da Justiça Sombria: Guerra de Apokolips</span>
+            <p>  Na trama, Batman reunia um grupo de heróis mágicos para lidar com ameaças sobrenaturais. Entre a galeria de heróis da 
+            Liga da Justiça Sombria, temos Constantine, Monstro do Pântano, Zatanna, Etrigan, entre outros. 
+            É aguardado a aparição de alguns personagens como Darkseid, as Fúrias e até mesmo o Pai Celestial.</p>
+          </div>
+        </div>
+    </div>
+
+
+
+
+
+<!--- Fim da Coluna Geral -->
 </div>
 
 </body>
