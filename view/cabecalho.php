694e0da1da9019185e072761d4b4d071dcd841d6
Raianwz <Raian03@live.com>
2020-07-20 21:24:30 -0300

    Removendo Noticies 🙌

--- view/cabecalho.php
+++ view/cabecalho.php
@@ -1,18 +1,15 @@
-
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="shortcut icon" href="ico/coin.svg" type="image/x-icon">
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <link rel="stylesheet" href="../style.css">
-    
-    <title>Clorocine</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Fliperama</title>
 </head>
--- view/cadastrar.php
+++ view/cadastrar.php
@@ -1,7 +1,8 @@
-<?php include "cabecalho.php" ?>
+<?php include "view/cabecalho.php" ?>
 
 <body>
-    <nav class="nav-extended purple lighten-3">
+
+    <nav class="nav-extended grey darken-2">
         <div class="nav-wrapper">
             <ul id="nav-mobile" class="right">
                 <li><a href="/">Galeria</a></li>
@@ -9,70 +10,68 @@
             </ul>
         </div>
         <div class="nav-header center">
-            <h1>CLOROCINE</h1>
+            <h1>FLIPERAMA</h1>
         </div>
-        <div class="nav-content purple darken-1">
+        <div class="nav-content black">
             <ul class="tabs tabs-transparent">
                 <li class="tab"><a class="active" href="#test1">Todos</a></li>
-                <li class="tab"><a href="#test2">Assistidos</a></li>
-                <li class="tab"><a href="#test3">Favoritos</a></li>
+                <li class="tab"><a href="#Jogados">Jogados</a></li>
+                <li class="tab"><a href="#Favoritos">Favoritos</a></li>
             </ul>
         </div>
     </nav>
 
     <div class="row">
-    <form method="POST" enctype="multipart/form-data">
-
-        <div class="col s6 offset-s3">
-            <div class="card">
-                <div class="card-content">
-                    <span class="card-title">Cadastrar Filme</span>
-                    <!-- Input do Título -->
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input id="titulo" type="text" class="validate" name="titulo" required>
-                            <label for="titulo">Título do Filme</label>
-                        </div>
-                    </div>
-
-                    <!-- Input da Sinopse -->
-                    <div class="row">
-                            <div class="row">
-                                <div class="input-field col s12">
-                                    <textarea name="sinopse" id="sinopse" class="materialize-textarea"></textarea>
-                                    <label for="sinopse">Sinopse</label>
-                                </div>
+        <form method="POST" enctype="multipart/form-data">
+            <div class="col s6 offset-s3">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title">Cadastrar Jogo</span>
+                        <!-- Input do Título -->
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="titulo" type="text" class="validate" name="titulo" require>
+                                <label for="titulo">Título do Jogo</label>
                             </div>
-                    </div>
-
-                    <!-- Input da Nota -->
-                    <div class="row">
-                        <div class="input-field col s4">
-                            <input name="nota" id="nota" type="number" step=".1" min="0" max="10" class="validate" required>
-                            <label for="nota">Nota</label>
                         </div>
-                    </div>
 
-                    <!-- Input Capa do Filme -->
+                        <!-- Input da Sinopse -->
+                        <div class="row">
+                            <form class="col s12">
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <textarea name="lore" id="lore" class="materialize-textarea"></textarea>
+                                        <label for="lore">Descrição</label>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
+                        <!-- Input da Nota -->
+                        <div class="row">
+                            <div class="input-field col s4">
+                                <input name="nota" id="nota" type="number" step=".1" min="0" max="10" class="validate" require>
+                                <label for="nota">Nota</label>
+                            </div>
+                        </div>
+
+                        <!-- Input Capa do Filme -->
                         <div class="file-field input-field">
-                            <div class="btn purple lighten-2 black-text">
+                            <div class="btn blue accent-1 black-text">
                                 <span>Capa</span>
-                                <input type="file" name="poster_file" accept="image/*">
+                                <input type="file" name="capa_file" accept="image/*">
                             </div>
                             <div class="file-path-wrapper">
-                                <input class="file-path validate" type="text" placeholder="Enviar Capa" name="poster">
+                                <input class="file-path validate" type="text" placeholder="Enviar Capa" name="capa">
                             </div>
                         </div>
-                </div>
-                <div class="card-action">
-                    <a class="btn waves-effect waves-light grey" href="/">Cancelar</a>
-                    <button type="submit" href="#" class="waves-effect waves-light btn purple">Confirmar</button>
+                    </div>
+                    <div class="card-action">
+                        <a class="btn waves-effect waves-light grey" href="/">Cancelar</a>
+                        <button type="submit" href="#" class="waves-effect waves-light btn blue accent-3">Confirmar</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </form>
+        </form>
     </div>
-
 </body>
-
-</html>
